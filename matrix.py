d3ee32e33ae5c9b59bb82b2d1453be68c356c585
Leo Vidarte <lvidarte@gmail.com>
2014-08-07 04:40:38 -0300

    First usable version - Added properties decorators on x, y, r, g and b - Added properties decorators on obj, page and reset - Moved constants to class body - Added debug funcion

--- matrix.py
+++ matrix.py
@@ -1,171 +1,243 @@
 #!/usr/bin/python -i
 
 import serial
-
 conn = serial.Serial('/dev/ttyACM0', 9600)
 
-CMD_CLEAR = 0b0000
-CMD_FLIP  = 0b0001
-CMD_SET_X = 0b1000
-CMD_SET_Y = 0b1001
-CMD_SET_R = 0b1010
-CMD_SET_G = 0b1011
-CMD_SET_B = 0b1100
-CMD_FILL  = 0b1111
+class Matrix(object):
 
-PARAM_OBJ_LED =   0b00
-PARAM_OBJ_COL =   0b01
-PARAM_OBJ_ROW =   0b10
-PARAM_OBJ_ALL =   0b11
-PARAM_PAGE_BG =  0b000
-PARAM_PAGE_FG =  0b100
-PARAM_NORST   = 0b0000
-PARAM_RESET   = 0b1000
+    CMD_CLEAR = 0b0000
+    CMD_FLIP  = 0b0001
+    CMD_SET_X = 0b1000
+    CMD_SET_Y = 0b1001
+    CMD_SET_R = 0b1010
+    CMD_SET_G = 0b1011
+    CMD_SET_B = 0b1100
+    CMD_DEBUG = 0b1110
+    CMD_FILL  = 0b1111
 
-MASK_POS   = 0b111
-MASK_COLOR = 0b1111
+    PARAM_OBJ_LED   =   0b00
+    PARAM_OBJ_COL   =   0b01
+    PARAM_OBJ_ROW   =   0b10
+    PARAM_OBJ_ALL   =   0b11
+    PARAM_PAGE_BG   =  0b000
+    PARAM_PAGE_FG   =  0b100
+    PARAM_NORST     = 0b0000
+    PARAM_RESET     = 0b1000
+    PARAM_DEBUG_OFF =    0b0
+    PARAM_DEBUG_ON  =    0b1
 
+    MASK_POS   = 0b0111
+    MASK_COLOR = 0b1111
 
-class Matrix:
+    PARAM_OBJ_NAMES = {
+        PARAM_OBJ_LED: 'led',
+        PARAM_OBJ_COL: 'col',
+    }
 
-    x = None
-    y = None
-    r = None
-    g = None
-    b = None
+    PARAM_PAGE_NAMES = {
+        PARAM_PAGE_BG: 'bg',
+        PARAM_PAGE_FG: 'fg',
+    }
+
+    PARAM_RESET_NAMES = {
+        PARAM_RESET: 'on',
+        PARAM_NORST: 'off',
+    }
 
     def __init__(self, conn):
         self.conn = conn
-        self.reset_internals()
+        self._x = None
+        self._y = None
+        self._r = None
+        self._g = None
+        self._b = None
+        self.reset_all()
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        if value is not None:
+            value &= self.MASK_POS
+            if self._x != value:
+                self._x = value
+                self.send(self.CMD_SET_X, value)
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        if value is not None:
+            value &= self.MASK_POS
+            if self._y != value:
+                self._y = value
+                self.send(self.CMD_SET_Y, value)
+
+    @property
+    def r(self):
+        return self._r
+
+    @r.setter
+    def r(self, value):
+        if value is not None:
+            value &= self.MASK_COLOR
+            if self._r != value:
+                self._r = value
+                self.send(self.CMD_SET_R, value)
+
+    @property
+    def g(self):
+        return self._g
+
+    @g.setter
+    def g(self, value):
+        if value is not None:
+            value &= self.MASK_COLOR
+            if self._g != value:
+                self._g = value
+                self.send(self.CMD_SET_G, value)
+
+    @property
+    def b(self):
+        return self._b
+
+    @b.setter
+    def b(self, value):
+        if value is not None:
+            value &= self.MASK_COLOR
+            if self._b != value:
+                self._b = value
+                self.send(self.CMD_SET_B, value)
 
     def set(self, x=None, y=None, r=None, g=None, b=None):
-        if x is not None:
-            self.set_x(x)
-        if y is not None:
-            self.set_y(y)
-        if r is not None:
-            self.set_r(r)
-        if g is not None:
-            self.set_g(g)
-        if b is not None:
-            self.set_b(b)
+        self.x = x
+        self.y = y
+        self.r = r
+        self.g = g
+        self.b = b
         self.fill()
 
-    def set_page_fg(self):
-        self.page = PARAM_PAGE_FG
+    def set0(self, x=0, y=0, r=0, g=0, b=0):
+        self.set(x, y, r, g, b)
 
-    def set_page_bg(self):
-        self.page = PARAM_PAGE_BG
+    @property
+    def obj(self):
+        return self._obj
 
     def set_obj_led(self):
-        self.obj = PARAM_OBJ_LED
+        self._obj = self.PARAM_OBJ_LED
 
     def set_obj_row(self):
-        self.obj = PARAM_OBJ_ROW
+        self._obj = self.PARAM_OBJ_ROW
 
     def set_obj_col(self):
-        self.obj = PARAM_OBJ_COL
+        self._obj = self.PARAM_OBJ_COL
 
     def set_obj_all(self):
-        self.obj = PARAM_OBJ_ALL
+        self._obj = self.PARAM_OBJ_ALL
+
+    @property
+    def page(self):
+        return self._page
+
+    def set_page_fg(self):
+        self._page = self.PARAM_PAGE_FG
+
+    def set_page_bg(self):
+        self._page = self.PARAM_PAGE_BG
+
+    @property
+    def reset(self):
+        return self._reset
 
     def set_reset_on(self):
-        self.reset = PARAM_RESET
+        self._reset = self.PARAM_RESET
 
     def set_reset_off(self):
-        self.reset = PARAM_NORST
-
-    def set_x(self, x):
-        x &= MASK_POS
-        if x != self.x:
-            self.x = x
-            self.send(CMD_SET_X, self.x)
-
-    def set_y(self, y):
-        y &= MASK_POS
-        if y != self.y:
-            self.y = y
-            self.send(CMD_SET_Y, self.y)
-
-    def set_r(self, r):
-        r &= MASK_COLOR
-        if r != self.r:
-            self.r = r
-            self.send(CMD_SET_R, self.r)
-
-    def set_g(self, g):
-        g &= MASK_COLOR
-        if g != self.g:
-            self.g = g
-            self.send(CMD_SET_G, self.g)
-
-    def set_b(self, b):
-        b &= MASK_COLOR
-        if b != self.b:
-            self.b = b
-            self.send(CMD_SET_B, self.b)
+        self._reset = self.PARAM_NORST
 
     def flip(self):
-        self.send(CMD_FLIP, 0)
+        self.send(self.CMD_FLIP, 0)
 
     def fill(self, obj=None, page=None, reset=None):
-        obj   = self.get_obj(obj)
-        page  = self.get_page(page)
-        reset = self.get_reset(reset)
+        obj   = self._get_obj(obj)
+        page  = self._get_page(page)
+        reset = self._get_reset(reset)
         param = obj | page | reset
-        self.send(CMD_FILL, param)
+        self.send(self.CMD_FILL, param)
 
     def clear(self, obj=None, page=None, reset=None):
-        obj   = self.get_obj(obj)
-        page  = self.get_page(page)
-        reset = self.get_reset(reset)
+        obj   = self._get_obj(obj)
+        page  = self._get_page(page)
+        reset = self._get_reset(reset)
         param = obj | page | reset
-        self.send(CMD_CLEAR, param)
+        self.send(self.CMD_CLEAR, param)
 
-    def get_obj(self, obj=None):
+    def _get_obj(self, obj=None):
         if obj is None:
             return self.obj
         else:
             return obj
 
-    def get_reset(self, reset=None):
+    def _get_reset(self, reset=None):
         if reset is None:
             return self.reset
         else:
             return reset
 
-    def get_page(self, page=None):
+    def _get_page(self, page=None):
         if page is None:
             return self.page
         else:
             return page
 
-    def reset_internals(self):
-        self.set_x(0)
-        self.set_y(0)
-        self.set_r(0)
-        self.set_g(0)
-        self.set_b(0)
+    def reset_matrix(self):
+        self.x = 0
+        self.y = 0
+        self.r = 0
+        self.g = 0
+        self.b = 0
         self.set_obj_led()
         self.set_page_fg()
         self.set_reset_off()
 
-    def reset2(self):
-        self.reset_internals()
-        self.fill(PARAM_OBJ_ALL, PARAM_PAGE_FG, PARAM_RESET)
-        self.fill(PARAM_OBJ_ALL, PARAM_PAGE_BG, PARAM_NORST)
+    def reset_page_bg(self):
+        self.clear(self.PARAM_OBJ_ALL, self.PARAM_PAGE_BG)
+
+    def reset_page_fg(self):
+        self.clear(self.PARAM_OBJ_ALL, self.PARAM_PAGE_FG)
+
+    def reset_all(self):
+        self.reset_matrix()
+        self.reset_page_bg()
+        self.reset_page_fg()
+
+    def set_debug_on(self):
+        self.send(self.CMD_DEBUG, self.PARAM_DEBUG_ON)
+
+    def set_debug_off(self):
+        self.send(self.CMD_DEBUG, self.PARAM_DEBUG_OFF)
 
     def send(self, command, param):
         self.msg = chr((command << 4) + param)
         self.conn.write(self.msg)
 
+    def __repr__(self):
+        return self.__str__()
+
     def __str__(self):
-        s = "xy(%s, %s) rgb(%s, %s, %s) obj=%s page=%s reset=%s"
-        return s % (self.x, self.y, self.r, self.g, self.b,
-                    self.obj, self.page, self.reset)
+        obj = self.PARAM_OBJ_NAMES[self.obj]
+        page = self.PARAM_PAGE_NAMES[self.page]
+        reset = self.PARAM_RESET_NAMES[self.reset]
+        return "xy(%s, %s) rgb(%s, %s, %s) obj:%s page:%s reset:%s" % (
+                self.x, self.y, self.r, self.g, self.b, 
+                obj, page, reset
+               )
 
-
-class Led:
     def __init__(self, x=0, y=0, r=0, g=0, b=0):
         self.x = x
         self.y = y
@@ -189,12 +261,15 @@ class Led:
 
 
 if __name__ == '__main__':
+
+    # Shell mode
+
     import atexit
     import os
     import readline
     import rlcompleter
 
-    historyPath = os.path.expanduser("~/.pyhistory")
+    historyPath = os.path.expanduser("~/.matrix-history")
 
     #readline.parse_and_bind('tab: menu-complete')
     readline.parse_and_bind('tab: complete')
