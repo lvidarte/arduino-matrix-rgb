fb832f902f51d902f774f70b8fb8424b98947a98
Leonardo Vidarte <lvidarte@gmail.com>
2014-08-12 10:48:29 -0300

    Refactoring

--- matrix.py
+++ matrix.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python -i
 
+import sys
 import time
 import random
 import serial
 
 try:
     conn = serial.Serial('/dev/ttyACM0', 9600)
-except:
-    conn = serial.Serial('/dev/ttyACM1', 9600)
+except OSError:
+    try:
+        conn = serial.Serial('/dev/ttyACM1', 9600)
+    except OSError:
+        print "Can't connect to serial device"
+        sys.exit(1)
 
 class Matrix(object):
 
@@ -71,7 +76,7 @@ class Matrix(object):
             value &= self.MASK_POS
             if self._x != value:
                 self._x = value
-                self.send(self.CMD_SET_X, value)
+                self._send(self.CMD_SET_X, value)
 
     @property
     def y(self):
@@ -83,7 +88,7 @@ class Matrix(object):
             value &= self.MASK_POS
             if self._y != value:
                 self._y = value
-                self.send(self.CMD_SET_Y, value)
+                self._send(self.CMD_SET_Y, value)
 
     @property
     def r(self):
@@ -95,7 +100,7 @@ class Matrix(object):
             value &= self.MASK_COLOR
             if self._r != value:
                 self._r = value
-                self.send(self.CMD_SET_R, value)
+                self._send(self.CMD_SET_R, value)
 
     @property
     def g(self):
@@ -107,7 +112,7 @@ class Matrix(object):
             value &= self.MASK_COLOR
             if self._g != value:
                 self._g = value
-                self.send(self.CMD_SET_G, value)
+                self._send(self.CMD_SET_G, value)
 
     @property
     def b(self):
@@ -119,22 +124,22 @@ class Matrix(object):
             value &= self.MASK_COLOR
             if self._b != value:
                 self._b = value
-                self.send(self.CMD_SET_B, value)
+                self._send(self.CMD_SET_B, value)
 
     def set_obj_led(self):
-        """Sets the actual object to LED"""
+        """Sets the actual object to 'led'"""
         self._obj = self.PARAM_OBJ_LED
 
     def set_obj_row(self):
-        """Sets the actual object to ROW"""
+        """Sets the actual object to 'row'"""
         self._obj = self.PARAM_OBJ_ROW
 
     def set_obj_col(self):
-        """Sets the actual object to COL"""
+        """Sets the actual object to 'col'"""
         self._obj = self.PARAM_OBJ_COL
 
     def set_obj_all(self):
-        """Sets the actual object to ALL"""
+        """Sets the actual object to 'all'"""
         self._obj = self.PARAM_OBJ_ALL
 
     def set_page_fg(self):
@@ -146,18 +151,19 @@ class Matrix(object):
         self._page = self.PARAM_PAGE_BG
 
     def set_reset_on(self):
-        """Sets the reset mode ON to put on 0
-           all arduino state (xyrgb) after writing"""
+        """Sets the reset mode on
+           for put on 0 the arduino xyrgb state
+           after fill or clear action"""
         self._reset = self.PARAM_RESET_ON
 
     def set_reset_off(self):
-        """Sets the reset mode OFF.
-           Look help on set_reset_on"""
+        """Sets the reset mode off.
+           See set_reset_on help"""
         self._reset = self.PARAM_RESET_OFF
 
     def flip(self):
         """Flip the page to change background by foreground"""
-        self.send(self.CMD_FLIP, 0)
+        self._send(self.CMD_FLIP, 0)
 
     def set_rgb(self, r=None, g=None, b=None):
         """Sets the rgb state"""
@@ -183,14 +189,6 @@ class Matrix(object):
         """Sets randomly the y position"""
         self.y = random.randint(0, 7)
 
-    def fill(self, obj=None, page=None, reset=None):
-        """Fill the object with the actual state (xyrgb)"""
-        obj   = self._get_obj(obj)
-        page  = self._get_page(page)
-        reset = self._get_reset(reset)
-        param = obj | page | reset
-        self.send(self.CMD_FILL, param)
-
     def set(self, x=None, y=None, r=None, g=None, b=None):
         """Sets the state (xyrgb) and fill the actual object"""
         self.x = x
@@ -205,161 +203,184 @@ class Matrix(object):
            0 by default"""
         self.fill(x, y, r, g, b)
 
-    def set_led(self):
+    def fill(self, obj=None, page=None, reset=None):
+        """Fill the object with the actual state (xyrgb)"""
+        param = self._get_param(obj, page, reset)
+        self._send(self.CMD_FILL, param)
+
+    def fill_led(self):
+        """Fill the object led on the actual page
+           with the actual xyrgb state"""
         self.fill(self.PARAM_OBJ_LED,
-                  self._page,
-                  self.PARAM_RESET_OFF)
+                  self._page)
 
-    def set_led_bg(self):
+    def fill_led_bg(self):
+        """Fill the object led on the background page
+           with the actual xyrgb state"""
         self.fill(self.PARAM_OBJ_LED,
-                  self.PARAM_PAGE_BG,
-                  self.PARAM_RESET_OFF)
+                  self.PARAM_PAGE_BG)
 
-    def set_led_fg(self):
+    def fill_led_fg(self):
+        """Fill the object led on the foreground page
+           with the actual xyrgb state"""
         self.fill(self.PARAM_OBJ_LED,
-                  self.PARAM_PAGE_FG,
-                  self.PARAM_RESET_OFF)
+                  self.PARAM_PAGE_FG)
 
-    def set_col(self):
+    def fill_col(self):
+        """Fill the object col on the actual page
+           with the actual xrgb state"""
         self.fill(self.PARAM_OBJ_COL,
-                  self._page,
-                  self.PARAM_RESET_OFF)
+                  self._page)
 
-    def set_col_bg(self):
+    def fill_col_bg(self):
+        """Fill the object col on the background page
+           with the actual xrgb state"""
         self.fill(self.PARAM_OBJ_COL,
-                  self.PARAM_PAGE_BG,
-                  self.PARAM_RESET_OFF)
+                  self.PARAM_PAGE_BG)
 
-    def set_col_fg(self):
+    def fill_col_fg(self):
+        """Fill the object col on the foreground page
+           with the actual xrgb state"""
         self.fill(self.PARAM_OBJ_COL,
-                  self.PARAM_PAGE_FG,
-                  self.PARAM_RESET_OFF)
+                  self.PARAM_PAGE_FG)
 
-    def set_row(self):
+    def fill_row(self):
+        """Fill the object row on the actual page
+           with the actual yrgb state"""
         self.fill(self.PARAM_OBJ_ROW,
-                  self._page,
-                  self.PARAM_RESET_OFF)
+                  self._page)
 
-    def set_row_bg(self):
+    def fill_row_bg(self):
+        """Fill the object row on the background page
+           with the actual yrgb state"""
         self.fill(self.PARAM_OBJ_ROW,
-                  self.PARAM_PAGE_BG,
-                  self.PARAM_RESET_OFF)
+                  self.PARAM_PAGE_BG)
 
-    def set_row_fg(self):
+    def fill_row_fg(self):
+        """Fill the object row on the foreground page
+           with the actual yrgb state"""
         self.fill(self.PARAM_OBJ_ROW,
-                  self.PARAM_PAGE_FG,
-                  self.PARAM_RESET_OFF)
+                  self.PARAM_PAGE_FG)
 
-    def set_all(self):
+    def fill_all(self):
+        """Fill the object all on the actual page
+           with the actual rgb state"""
         self.fill(self.PARAM_OBJ_ALL,
-                  self._page,
-                  self.PARAM_RESET_OFF)
+                  self._page)
 
-    def set_all_bg(self):
+    def fill_all_bg(self):
+        """Fill the object all on the background page
+           with the actual rgb state"""
         self.fill(self.PARAM_OBJ_ALL,
-                  self.PARAM_PAGE_BG,
-                  self.PARAM_RESET_OFF)
+                  self.PARAM_PAGE_BG)
 
-    def set_all_fg(self):
+    def fill_all_fg(self):
+        """Fill the object all on the foreground page
+           with the actual rgb state"""
         self.fill(self.PARAM_OBJ_ALL,
-                  self.PARAM_PAGE_FG,
-                  self.PARAM_RESET_OFF)
+                  self.PARAM_PAGE_FG)
 
     def clear(self, obj=None, page=None, reset=None):
-        obj   = self._get_obj(obj)
-        page  = self._get_page(page)
-        reset = self._get_reset(reset)
-        param = obj | page | reset
-        self.send(self.CMD_CLEAR, param)
+        """Clear the object"""
+        param = self._get_param(obj, page, reset)
+        self._send(self.CMD_CLEAR, param)
 
-    def off(self):
-        self.clear()
-
-    def off_led(self):
+    def clear_led(self):
+        """Clear the object led on the actual page"""
         self.clear(self.PARAM_OBJ_LED,
-                   self._page,
-                   self.PARAM_RESET_OFF)
+                   self._page)
 
-    def off_led_bg(self):
+    def clear_led_bg(self):
+        """Clear the object led on the background page"""
         self.clear(self.PARAM_OBJ_LED,
-                   self.PARAM_PAGE_BG,
-                   self.PARAM_RESET_OFF)
+                   self.PARAM_PAGE_BG)
 
-    def off_led_fg(self):
+    def clear_led_fg(self):
+        """Clear the object led on the foreground page"""
         self.clear(self.PARAM_OBJ_LED,
-                   self.PARAM_PAGE_FG,
-                   self.PARAM_RESET_OFF)
+                   self.PARAM_PAGE_FG)
 
-    def off_col(self):
+    def clear_col(self):
+        """Clear the object col on the actual page"""
         self.clear(self.PARAM_OBJ_COL,
-                   self._page,
-                   self.PARAM_RESET_OFF)
+                   self._page)
 
-    def off_col_bg(self):
+    def clear_col_bg(self):
+        """Clear the object col on the background page"""
         self.clear(self.PARAM_OBJ_COL,
-                   self.PARAM_PAGE_BG,
-                   self.PARAM_RESET_OFF)
+                   self.PARAM_PAGE_BG)
 
-    def off_col_fg(self):
+    def clear_col_fg(self):
+        """Clear the object col on the foreground page"""
         self.clear(self.PARAM_OBJ_COL,
-                   self.PARAM_PAGE_FG,
-                   self.PARAM_RESET_OFF)
+                   self.PARAM_PAGE_FG)
 
-    def off_row(self):
+    def clear_row(self):
+        """Clear the object row on the actual page"""
         self.clear(self.PARAM_OBJ_ROW,
-                   self._page,
-                   self.PARAM_RESET_OFF)
+                   self._page)
 
-    def off_row_bg(self):
+    def clear_row_bg(self):
+        """Clear the object row on the background page"""
         self.clear(self.PARAM_OBJ_ROW,
-                   self.PARAM_PAGE_BG,
-                   self.PARAM_RESET_OFF)
+                   self.PARAM_PAGE_BG)
 
-    def off_row_fg(self):
+    def clear_row_fg(self):
+        """Clear the object row on the foreground page"""
         self.clear(self.PARAM_OBJ_ROW,
-                   self.PARAM_PAGE_FG,
-                   self.PARAM_RESET_OFF)
+                   self.PARAM_PAGE_FG)
 
-    def off_all(self):
+    def clear_all(self):
+        """Clear the object all on the actual page"""
         self.clear(self.PARAM_OBJ_ALL,
-                   self._page,
-                   self.PARAM_RESET_OFF)
+                   self._page)
 
-    def off_all_bg(self):
+    def clear_all_bg(self):
+        """Clear the object all on the background page"""
         self.clear(self.PARAM_OBJ_ALL,
-                   self.PARAM_PAGE_BG,
-                   self.PARAM_RESET_OFF)
+                   self.PARAM_PAGE_BG)
 
-    def off_all_fg(self):
+    def clear_all_fg(self):
+        """Clear the object all on the foreground page"""
         self.clear(self.PARAM_OBJ_ALL,
-                   self.PARAM_PAGE_FG,
-                   self.PARAM_RESET_OFF)
+                   self.PARAM_PAGE_FG)
 
-    def rect(self, x=None, y=None, w=3, h=3):
+    def rect(self, width=3, height=3, x=None, y=None):
+        """Draw a rectangle"""
         x = x or self._x
         y = y or self._y
-        for n in range(w):
+        for n in range(width):
             _x = x + n
-            _y = y + h - 1
+            _y = y + height - 1
             self.set(x=_x, y=y)
             if y != _y:
                 self.set(x=_x, y=_y)
-        for m in range(h-2):
-            _x = x + w - 1
+        for m in range(height - 2):
+            _x = x + width - 1
             _y = y + m + 1
             self.set(x=x, y=_y)
             if x != _x:
                 self.set(x=_x, y=_y) 
         self.x, self.y = x, y
 
-    def square(self, x=None, y=None, size=3):
-        self.rect(x, y, size, size)
+    def square(self, size=3, x=None, y=None):
+        """Draw a square"""
+        self.rect(size, size, x, y)
 
-    def _get_value(self, name, value=None):
-        if value is None:
-            return getattr(self, '_%s' % name)
-        else:
-            return value
+    def line_horizontal(self, size=5, x=None, y=None):
+        """Draw a horizontal line"""
+        self.rect(width=size, height=1, x=x, y=y)
+
+    def line_vertical(self, size=5, x=None, y=None):
+        """Draw a vertical line"""
+        self.rect(width=1, height=size, x=x, y=y)
+
+    def _get_param(self, obj, page, reset):
+        obj   = self._get_obj(obj)
+        page  = self._get_page(page)
+        reset = self._get_reset(reset)
+        param = obj | page | reset
+        return param
 
     def _get_obj(self, obj=None):
         return self._get_value('obj', obj)
@@ -370,7 +391,14 @@ class Matrix(object):
     def _get_reset(self, reset=None):
         return self._get_value('reset', reset)
 
+    def _get_value(self, name, value=None):
+        if value is None:
+            return getattr(self, '_%s' % name)
+        else:
+            return value
+
     def reset(self):
+        """Reset the matrix to initial status"""
         self.x = 0
         self.y = 0
         self.r = 0
@@ -379,16 +407,18 @@ class Matrix(object):
         self.set_obj_led()
         self.set_page_fg()
         self.set_reset_off()
-        self.off_all_bg()
-        self.off_all_fg()
+        self.clear_all_bg()
+        self.clear_all_fg()
 
     def set_debug_on(self):
-        self.send(self.CMD_DEBUG, self.PARAM_DEBUG_ON)
+        """Set on verbosity on arduino"""
+        self._send(self.CMD_DEBUG, self.PARAM_DEBUG_ON)
 
     def set_debug_off(self):
-        self.send(self.CMD_DEBUG, self.PARAM_DEBUG_OFF)
+        """Set off verbosity on arduino"""
+        self._send(self.CMD_DEBUG, self.PARAM_DEBUG_OFF)
 
-    def send(self, command, param):
+    def _send(self, command, param):
         self.msg = chr((command << 4) + param)
         self.conn.write(self.msg)
 
@@ -442,21 +472,21 @@ if __name__ == '__main__':
             m.set_rand_x()
             m.set_rand_y()
             m.set_rand_rgb()
-            m.fill()
+            m.fill_led()
             time.sleep(sec)
 
     def random_dots_forever_fg(sec=.1, amount=10):
         """Draw forever dots on random position on foreground, random color"""
         m.set_page_fg()
         while True:
-            m.off_all()
+            m.clear_all()
             random_dots(sec, amount)
 
     def random_dots_forever_bg(sec=.1, amount=10):
         """Draw forever dots on random position on background, random color"""
         m.set_page_bg()
         while True:
-            m.off_all_bg()
+            m.clear_all_bg()
             random_dots(sec, amount)
             m.flip();
 
@@ -563,7 +593,7 @@ if __name__ == '__main__':
         m.set_page_bg()
         m.set_rand_rgb()
         for i in range(4):
-            m.off_all_bg()
+            m.clear_all_bg()
             m.x = 3 - i
             m.y = 3 - i
             m.square(size=(i+1)*2)
