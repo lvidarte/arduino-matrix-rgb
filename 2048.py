db430eedf15b54557bd1599e411ceb98ec24d54c
Leo Vidarte <lvidarte@gmail.com>
2014-12-12 07:11:44 -0300

    First usable version of 2048

--- 2048.py
+++ 2048.py
@@ -1,32 +1,100 @@
 #!/bin/python
 
 import random
+import time
+
+
+class Getch:
+
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
 
 class Arduino2048:
 
-    def __init__(self):
+    def __init__(self, size=4):
+        self.size = 4
         self.start()
 
     def start(self):
         self.reset()
+        self.add()
+        self.add()
+        commands = {'h': 'left', 'j': 'bottom', 'k': 'top', 'l': 'right'}
+        getch = Getch()
+        while True:
+            print '-' * 10
+            print self
+            c = getch()
+            self.move(commands[c])
 
     def reset(self):
-        self.board = [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0]
-        ]
+        self.board = [[0 for i in range(self.size)]
+                      for j in range(self.size)]
 
-    def move(self):
-        self.added = False
-        self.move_left()
-        if self.added == False:
+    def move(self, direction):
+        moves = {
+            'left'  : self.move_left,
+            'right' : self.move_right,
+            'top'   : self.move_top,
+            'bottom': self.move_bottom,
+        }
+        board = moves[direction]()
+        if board != self.board:
+            self.board = board
+            print '-' * 10
+            print self
+            time.sleep(.25)
             self.add()
+            print '-' * 10
+            print self
 
     def move_left(self):
+        board = []
         for i, _ in enumerate(self.board):
-            self.board[i] = self.row_to_left(self.board[i])
+            row = self.row_to_left(self.board[i][:])
+            board.append(row)
+        return board
+
+    def move_right(self):
+        board = []
+        for i, _ in enumerate(self.board):
+            row = self.row_to_left(self.board[i][::-1])
+            board.append(row[::-1])
+        return board
+
+    def move_top(self):
+        actual_board = self.get_rotated_ccw(self.board)
+        board = []
+        for i, _ in enumerate(actual_board):
+            row = self.row_to_left(actual_board[i])
+            board.append(row)
+        return self.get_rotated_cw(board)
+
+    def move_bottom(self):
+        actual_board = self.get_rotated_cw(self.board)
+        board = []
+        for i, _ in enumerate(actual_board):
+            row = self.row_to_left(actual_board[i])
+            board.append(row)
+        return self.get_rotated_ccw(board)
+
+    def get_rotated_cw(self, board):
+        return [list(e) for e in zip(*board[::-1])]
+
+    def get_rotated_ccw(self, board):
+        return [list(e) for e in zip(*board)[::-1]]
 
     def row_to_left(self, row):
         for i in range(3):
@@ -46,7 +114,6 @@ class Arduino2048:
                 row.pop(i)
                 row[i] *= 2
                 row.append(0)
-                self.added = True
         return row
 
     def add(self):
@@ -57,8 +124,14 @@ class Arduino2048:
                     empty.append((x, y))
         if len(empty):
             x, y = random.choice(empty)
-            self.board[x][y] = 1
+            self.board[x][y] = 2
 
+    def __str__(self):
+        return "\n".join(["".join(["% 5s" % v for v in row]) 
+                         for row in self.board]).replace('0', '_')
+
+    def __repr__(self):
+        return self.__str__()
 
 if __name__ == '__main__':
 
